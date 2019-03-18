66bd70b42c3663551abd22080f57f7806f16a326
Francisco Javier Rios Bautista <fjrb44@gmail.com>
2019-03-18 08:43:44 +0100

    Creacion de las Foreign Key
    Se han creado las foreign keys y se han arreglado un par de fallos de la creacion de las tablas.

--- src/assets/bd/relaciones.sql
+++ src/assets/bd/relaciones.sql
@@ -1,5 +1,63 @@
-/*
-ALTER TABLE tablaFK ADD CONSTRAINT
-  fk_nombre_constraint (tabla_id) references tabla(id)
+/* FK TABLAS PRINCIPALES */
+
+ALTER TABLE video add CONSTRAINT
+  `fkCategoriaVideo` FOREIGN KEY (`categoriaId`) references categoria(`id`)
   on delete cascade;
-*/
+
+alter table video add CONSTRAINT
+  `fkUsuarioVideo`  FOREIGN KEY(`usuarioId`) references usuario(`id`)
+  on delete cascade;
+
+alter table usuario add CONSTRAINT
+  `fkIdiomaUsuario` FOREIGN KEY (`idiomaId`) references idioma(`id`)
+  on delete cascade;
+
+alter table lista add CONSTRAINT
+  `fkUsuarioLista` FOREIGN KEY (`usuarioId`) references usuario(`id`)
+  on delete cascade;
+
+/* FK TABLAS SECUNDARIAS */
+
+alter table suscribir add CONSTRAINT
+  `fkCanalSuscribir` FOREIGN KEY (`canalId`) references usuario(`id`)
+  on delete cascade;
+
+alter table suscribir add CONSTRAINT
+  `fkUsuarioSuscribir` FOREIGN KEY (`usuarioId`) references usuario(`id`)
+  on delete cascade;
+
+alter table pertenecer add CONSTRAINT
+  `fkListaPertenecer` FOREIGN KEY (`listaId`) references lista(`id`)
+  on delete cascade;
+
+alter table pertenecer add CONSTRAINT
+  `fkVideoPertenecer` FOREIGN KEY (`videoId`) references video(`id`)
+  on delete cascade;
+
+alter table etiquetar add CONSTRAINT
+  `fkVideoEtiquetar` FOREIGN KEY (`videoId`) references video(`id`)
+  on delete cascade;
+
+alter table etiquetar add CONSTRAINT
+  `fkEtiquetaEtiquetar` FOREIGN KEY (`etiquetaId`) references etiqueta(`id`)
+  on delete cascade;
+
+alter table ver add CONSTRAINT
+  `fkVideoVer` FOREIGN KEY (`videoId`) references video(`id`)
+  on delete cascade;
+
+alter table ver add CONSTRAINT
+  `fkUsuarioVer` FOREIGN KEY (`usuarioId`) references usuario(`id`)
+  on delete cascade;
+
+alter table comentar add CONSTRAINT
+  `fkComentarioComentar` FOREIGN KEY (`comentarioId`) references comentario(`id`)
+  on delete cascade;
+
+alter table comentar add CONSTRAINT
+  `fkUsuarioComentar` FOREIGN KEY (`usuarioId`) references usuario(`id`)
+  on delete cascade;
+
+alter table comentar add CONSTRAINT
+  `fkVideoComentar` FOREIGN KEY (`videoId`) references video(`id`)
+  on delete cascade;
--- src/assets/bd/tablas.sql
+++ src/assets/bd/tablas.sql
@@ -1,88 +1,112 @@
 /* TABLAS PRINCIPALES */
 
-CREATE TABLE IF NOT EXISTS video{
-  id int(255) not null auto_increment, /* Cambiar en un futuro */
-  descripcion varchar(),
-  nombre varchar(50) not null,
-  estado enum('public', 'private', 'shared') not null,
-  fechaSubida date() not null,
-  imagen varchar(255) not null,
+CREATE TABLE IF NOT EXISTS `video`(
+  `id` int(255) not null auto_increment, /* Cambiar en un futuro */
+  `descripcion` varchar(255),
+  `nombre` varchar(50) not null,
+  `estado` enum('public', 'private', 'shared') not null,
+  `fechaSubida` datetime not null,
+  `imagen` varchar(255) not null,
 
-  categoriaId int(255),
-  usuarioId int(255)
-}
+  `categoriaId` int(255),
+  `usuarioId` int(255),
 
-create table if not exists usuario{
+  PRIMARY KEY(`id`)
+);
+
+create table if not exists usuario(
   id int(255) not null auto_increment,
   nick varchar(255) not null,
   mail varchar(255) not null,
   password varchar(255) not null,
   logo varchar(255),
-  fechaCreacion date() not null,
+  fechaCreacion datetime not null,
 
-  idiomaId int(255)
-}
+  idiomaId int(255),
 
-create table if not exists listas{
+  PRIMARY KEY(`id`)
+);
+
+create table if not exists lista(
   id int(255) not null auto_increment,
   descripcion int(255),
   nombre varchar(50) not null,
-  fechaCreacion date() not null,
+  fechaCreacion datetime not null,
 
-  usuarioId int(255)
-}
+  usuarioId int(255),
 
-create table if not exists comentario{
+  PRIMARY KEY(`id`)
+);
+
+create table if not exists comentario(
   id int(255) not null auto_increment,
   texto varchar(255),
-  fecha date(),
-  baneado boolean
-}
+  fecha datetime,
+  baneado boolean,
 
-create table if not exists categoria{
+  PRIMARY KEY(`id`)
+);
+
+create table if not exists categoria(
   id int(255) not null auto_increment,
-  nombre varchar(255) not null
-}
+  nombre varchar(255) not null,
 
-create table if not exists etiqueta{
+  PRIMARY KEY(`id`)
+);
+
+create table if not exists etiqueta(
   id int(255) not null auto_increment,
-  nombre varchar(255) not null
-}
+  nombre varchar(255) not null,
 
-create table if not exists idioma{
+  PRIMARY KEY(`id`)
+);
+
+create table if not exists idioma(
   id int(255) not null auto_increment,
   nombreCorto varchar(2) not null,
-  nombre varchar(20)
-}
+  nombre varchar(20),
 
-/* TABLAS DEPENDIENTES */
+  PRIMARY KEY(`id`)
+);
 
-create table if not exists suscribir{
+/* TABLAS SECUNDARIAS */
+
+create table if not exists suscribir(
   canalId int(255) not null,
-  usuarioId int(255) not null
-}
+  usuarioId int(255) not null,
 
-create table if not exists pertenecer{
-  listasId int(255) not null,
-  videoId int(255) not null
-}
+  PRIMARY KEY(`canalID`, `usuarioId`)
+);
 
-create table if not exists etiquetar{
+create table if not exists pertenecer(
+  listaId int(255) not null,
   videoId int(255) not null,
-  etiquetaId int(255) not null
-}
 
-create table if not exists ver{
+  PRIMARY KEY(`listaId`, `videoId`)
+);
+
+create table if not exists etiquetar(
+  videoId int(255) not null,
+  etiquetaId int(255) not null,
+
+  PRIMARY KEY(`videoId`, `etiquetaId`)
+);
+
+create table if not exists ver(
   terminado boolean not null,
   segundos int(255) not null,
   valoracion int(1),
 
   videoId int(255) not null,
-  usuarioId int(255) not null
-}
+  usuarioId int(255) not null,
 
-create table if not exists comentar{
+  PRIMARY KEY(`videoId`, `usuarioId`)
+);
+
+create table if not exists comentar(
   comentarioId int(255),
   usuarioId int(255),
-  videoId int(255)
-}
+  videoId int(255),
+
+  PRIMARY KEY(`comentarioId`, `usuarioId`, `videoId`)
+);
